6f7dd0289438b90fc16d96d730ae61c45c6ff038
zonglina <zonglina@ibg100.com>
2017-06-26 14:59:29 +0800

    ListView

--- ios/ListViewDemo/AppDelegate.m
+++ ios/ListViewDemo/AppDelegate.m
@@ -29,7 +29,7 @@
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
   rootViewController.view = rootView;
-  self.window.rootViewController = rootViewController;
+  self.window.rootViewController =     rootViewController;
   [self.window makeKeyAndVisible];
   return YES;
 }
